1efdc4106d3e2ab3644b11d7084749866eea6b0e
Darren Ezra <darrenezra902@gmail.com>
2024-06-14 00:16:02 +0700

    social media update

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,23 +1,46 @@
-import { Link } from "@material-ui/core";
-
 export function SocialMedia() {
   return (
     <>
       <div className="flex flex-row gap-2 absolute z-2 h-auto left-2 top-2">
-        <Link to="https://wa.me/6281510254631" className="cursor-pointer">
+        <button
+          onClick={() => {
+            window.open("https://wa.me/6281510254631", "_blank");
+          }}
+          className="cursor-pointer"
+        >
           <img
             src="https://static-00.iconduck.com/assets.00/whatsapp-icon-509x512-c9csi2fs.png"
             alt=""
             className="w-10 hover:bg-white rounded p-1 duration-300 ease-in-out"
           />
-        </Link>
-        <Link to="https://github.com/RenderDek90" className="cursor-pointer">
+        </button>
+        <button
+          onClick={() => {
+            window.open("https://github.com/RenderDek90", "_blank");
+          }}
+          className="cursor-pointer"
+        >
           <img
             src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
             alt=""
             className="w-10 hover:bg-white rounded p-1 duration-300 ease-in-out"
           />
-        </Link>
+        </button>
+        <button
+          onClick={() => {
+            window.open(
+              "https://www.linkedin.com/in/darren-ezra-9090/",
+              "_blank"
+            );
+          }}
+          className="cursor-pointer"
+        >
+          <img
+            src="https://cdn.iconscout.com/icon/free/png-512/free-linkedin-46-189755.png?f=webp&w=256"
+            alt=""
+            className="w-10 hover:bg-white rounded p-1 duration-300 ease-in-out"
+          />
+        </button>
       </div>
     </>
   );
